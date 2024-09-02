be94395cbaae772380713f76d90b0bc1c2497685
NotTheFloor <charlesgthomas644@gmail.com>
2024-09-02 11:47:47 -0400

    Added cursor centering remaps and copy rebuffer remap

--- lua/chunk/remap.lua
+++ lua/chunk/remap.lua
@@ -1,1 +1,10 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Cursor centering
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Delete rebuffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
